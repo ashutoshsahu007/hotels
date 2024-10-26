ae8af0c98248479afd4dede01f6418ca87007501
ashutosh sahu <ppaskumar@gmail.com>
2024-10-26 16:22:24 +0530

    Initail Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.end("App Running successfully 2 ");
+});
+
+app.listen(3000, () => {
+  console.log("port 3000");
+});
